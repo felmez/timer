f15aac49e9221c23ef7ddfba9498157216d06981
Souhaib <sofelmez@gmail.com>
2020-08-07 18:19:50 +0300

    project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Base/>
+  </div>
+</template>
+
+<script>
+import Base from "./components/Base";
+
+export default {
+  name: "App",
+
+  components: {
+    Base
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #fff;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+</style>
